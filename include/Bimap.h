d44a42b3370edf2325f8d0a20058bb97179bf8ed
foo <foo@bar>
2020-01-31 10:41:27 +0100

    added iterator

--- include/Bimap.h
+++ include/Bimap.h
@@ -10,10 +10,10 @@
 #include <memory>
 #include <iostream>
 #include <map>
+#include <iterator>
 #include <model/Data.h>
 #include <Coordinate.h>
 
-std::ostream& operator<<(std::ostream& os, const Coordinate& coordinate);
 
 template <typename Key, typename Value>
 using Map = std::map<Key, Value>;
@@ -30,7 +30,59 @@ class Bimap {
 private:
     Map<Key*, Value> forward;
     Map<Value, std::unique_ptr<Key>> backward;
-public:
+    public:
+
+    template<bool isconst>
+    class Iterator {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using size_type = std::size_t;
+        using value_type = std::pair<Key, Value>;
+        using pointer = typename std::conditional<isconst, const value_type *, value_type *>::type;
+        using reference = typename std::conditional<isconst, std::pair<const Key&, const Value&>, std::pair<Key&, Value&> >::type;
+        using innerIteratorType =  typename std::conditional<isconst, typename decltype(forward)::const_iterator, typename decltype(forward)::iterator>::type;
+        friend class Iterator<!isconst>;
+        Iterator(innerIteratorType it ) : iterator(it) {};
+
+        Iterator<isconst>& operator++() {
+            ++iterator;
+            return *this;
+        }
+
+        Iterator<isconst>& operator++(int) {
+            Iterator<isconst> tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        Iterator<isconst>& operator--() {
+            --iterator;
+            return *this;
+        }
+
+        Iterator<isconst>&operator--(int) {
+            Iterator<isconst> tmp = *this;
+            --(*this);
+            return tmp;
+        }
+
+        bool operator!=(Iterator<isconst>& rhs) {
+            return this->iterator != rhs.iterator;
+        }
+
+        reference operator*() {
+            using f1 = typename std::conditional<isconst, const Key&, Key&>::type;
+            using f2 = typename std::conditional<isconst, const Value&, Value&>::type;
+            return std::pair<f1, f2>(*iterator->first, iterator->second);
+        }
+
+    private:
+         innerIteratorType iterator;
+    };
+
+    using iterator = Iterator<true>;
+    using const_iterator = Iterator<false>;
 
     Key& get(const Value& value) {
         return *findInMap(value, backward);
@@ -82,6 +134,22 @@ public:
     void clear() {
         forward.clear();
         backward.clear();
+    }
+
+    iterator begin() {
+        return iterator(forward.begin());
+    }
+
+    const_iterator begin() const {
+        return const_iterator(forward.begin());
+    }
+
+    iterator end() {
+        return iterator(forward.end());
+    }
+
+    const_iterator end() const {
+        return const_iterator(forward.end());
     }
 };
 
